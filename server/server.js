ef516e2e62c724204b1483d0d3d0ded6e94ccd7d
Styles <stylesk11@gmail.com>
2024-03-27 11:49:47 -0700

    add apicache middleware

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+// const apicache = require('apicache');
 const path = require('path');
 const qaController = require('./controllers/index.js');
 
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../dist')));
+// let cache = apicache.middleware;
+// app.use(cache('30 seconds'));
 
 const port = process.env.PORT || 3000;
 
